a7e2a0d3afafed82a78fe9b257f9e6f278c27b79
Marvelous Johnson <jmarvelous437@gmail.com>
2023-03-07 08:24:34 +0100

    'first'

--- learning_templates/learning_templates/settings.py
+++ learning_templates/learning_templates/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-1cf4tm_49r@p3hm@1gpfhj@@1@ppl^p-y5i7&3ha&@ekczvoa&
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['1234lynch.pythonanywhere.com'],
+ALLOWED_HOSTS = ['*'],
 
 
 # Application definition
